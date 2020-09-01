2fea450c3dffaddc7a52aec988457781eb8cfcad
Duke Nukem <garrettduke87@gmail.com>
2020-09-01 00:47:34 -0600

    added event listener, and got value from input element

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,8 +1,36 @@
 // this function is called getUserRepos
-var getUserRepos = function() {
-    // fetches information from this url
-    fetch("https://api.github.com/users/octocat/repos");
-
+var getUserRepos = function (user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+        // formats the response as JSON "then" captures the actual data
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
 };
 
-getUserRepos();
+
+// this variable is the element taken from the "user-form" container in html
+var userFormEl = document.querySelector("#user-form");
+// this variable is the element taken from the "username" input in the html
+var nameInputEl = document.querySelector("#username");
+
+// prevents the page from refreshing when the button is clicked
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    
+};
+
+// the event listener takes the "user-form" when submitted 
+userFormEl.addEventListener("submit", formSubmitHandler);
